29c48f5601e82f2d4a10fb1c40e6a7acf1f62d74
Paul Miller <jettero@gmail.com>
2010-02-17 17:56:31 -0500

    got the about page essentially finished

--- app/assistants/Accounts.js
+++ app/assistants/Accounts.js
@@ -1,3 +1,13 @@
 function AccountsAssistant() {
     Mojo.Log.info("Accounts()");
 }
+
+// AccountsAssistant.prototype.setup = function() {{{
+AccountsAssistant.prototype.setup = function() {
+    Mojo.Log.info("Accounts::setup()");
+
+    this.SC = Mojo.Controller.stageController.assistant;
+    this.menuSetup = this.SC.menuSetup.bind(this);
+    this.menuSetup();
+}
+// }}}
