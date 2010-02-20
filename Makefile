6d5a86cc179abaabf3fcaff6924bfa1552bb9a65
Paul Miller <jettero@gmail.com>
2010-02-20 07:29:07 -0500

    there were bugs in log-parse and I added features to make sure I'm showing the whole run

--- Makefile
+++ Makefile
@@ -7,7 +7,7 @@ test: clean
 	@+HM_LOGLEVEL=99 make --no-print-directory build
 	palm-install *.ipk
 	$(ssh) luna-send -n 1 palm://com.palm.applicationManager/launch "'{\"id\":\"org.voltar.hiveminder\"}'"
-	$(ssh) tail -f /var/log/messages | ./log-parse.pl
+	$(ssh) tail -n 1000 -f /var/log/messages | ./log-parse.pl
 
 myinstall: clean
 	@+HM_LOGLEVEL=0 make --no-print-directory build
--- log-parse.pl
+++ log-parse.pl
@@ -7,8 +7,15 @@ use POSIX qw(strftime);
 my $old = select STDIN; $| = 1;
 select $old; $| = 1;
 
+my @start = ( map { strftime('%H:%M:%S', gmtime(time() + $_)) } (0,-1,+1) );
+my $start = do { local $" = "|"; qr(@start) };
+
+my $on = 0;
+
 while(<STDIN>) {
-    if( s/(\d{4})-(\d{2})-(\d{2})T(\d{2}:\d{2}:\d{2}).+?LunaSysMgr.*?{LunaSysMgrJS}:\s+// ) {
+    $on = 1 if m/$start.*?Info.*?loaded\(\w+\.js\)/;
+
+    if( s/(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}).+?LunaSysMgr.*?{LunaSysMgrJS}:\s+// ) {
         my ($year, $month, $day, $hour, $min, $sec) = ($1, $2, $3, $4, $5, $6);
         $month =~ s/^0//; $month --;
         my $esec = timegm_nocheck($sec,$min,$hour,$day,$month,$year);
@@ -16,7 +23,7 @@ while(<STDIN>) {
 
         s(, file://.*)();
 
-        print "$time: $_";
+        print "$time: $_" if $on;
     }
 }
 
