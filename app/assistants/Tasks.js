e6b3c71f7fdaa83fd9b7f6ee8193ba18d9b9155d
Paul Miller <jettero@gmail.com>
2010-02-24 06:58:34 -0500

    I need to notify cards that deal with logins differently

--- app/assistants/Tasks.js
+++ app/assistants/Tasks.js
@@ -10,6 +10,21 @@ function TasksAssistant() {
     Mojo.Log.info("Tasks::setup()");
 
     this.menuSetup();
+
+    this.commandMenuModel = { label: $L('Tasks Command Menu'), 
+        items: [{},{},{label: "cool", hidden: true, submenu:'login-submenu'}]
+    };		
+	this.controller.setupWidget(Mojo.Menu.commandMenu, undefined, this.commandMenuModel);		
+
+    this.loginSubmenu = { label: $L('Login Submenu'),
+        items: [
+            {label: "test1", command: "test1"},
+            {label: "test2", command: "test2"},
+            {label: "test3", command: "test3"},
+            {label: "test4", command: "test4"}
+        ]
+    };
+	this.controller.setupWidget('login-submenu', undefined, this.loginSubmenu);	
 };
 
 /*}}}*/
