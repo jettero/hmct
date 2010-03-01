39dfffbafda5a678de39cb94fc6151fad499f47c
Paul Miller <jettero@gmail.com>
2010-03-01 06:55:31 -0500

    ok, with this registered with the account manager, it's nearly time to start loading tasks

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -4,6 +4,16 @@ function TaskManager() {
 
     Mojo.Log.info("test-md5: %s, %s", hex_md5(""), hex_md5("abc"));
 
+    this.handleLoginChange = this.handleLoginChange.bind(this);
+
+    AMO.registerLoginChange(this.handleLoginChange);
 }
+
+TaskManager.prototype.handleLoginChange = function(emails,current) {
+    Mojo.Log.info("TaskManager::handleLoginChange(current=%s)", current);
+
+};
+
+/*}}}*/
 
 Mojo.Log.info('loaded(TaskManager.js)');
