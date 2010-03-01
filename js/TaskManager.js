d6f6504dbae745c3eddfec35de2ac31a9f7d533c
Paul Miller <jettero@gmail.com>
2010-03-01 18:38:08 -0500

    hrm, this is kindof how it'll look I think... I can't figure out how to actually search tasks though.  I have tokens from my lists, but no idea where to plug them in

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -5,6 +5,7 @@ function TaskManager() {
     Mojo.Log.info("test-md5: %s, %s", hex_md5(""), hex_md5("abc"));
 
     this.handleLoginChange = this.handleLoginChange.bind(this);
+    this.currentSearch = {};
 
     AMO.registerLoginChange(this.handleLoginChange);
 }
@@ -14,6 +15,68 @@ TaskManager.prototype.handleLoginChange = function(emails,current) {
 
 };
 
-/*}}}*/
+TaskManager.prototype.searchTasks = function() {
+    Mojo.Log.info("TaskManager::searchTasks()");
+
+    var request = new Ajax.Request('http://hiveminder.com/=/action/BTDT.Action.TaskSearch.json', {
+        method: 'post', parameters: this.currentSearch, evalJSON: true,
+
+        onSuccess: function(transport) {
+            if( transport.status == 200 ) {
+                var r = transport.responseJSON;
+                if( r ) {
+                    if( r.success ) {
+                        Mojo.Log.info("TaskManager::searchTasks() r.success r=%s", Object.toJSON(r));
+                        this.data.tasks = r;
+                        this.dbChanged("fetched tasks");
+
+                    } else {
+                        Mojo.Log.info("TaskManager::searchTasks() r.fail, r=%s", Object.toJSON(r));
+                        var e = [];
+
+                        if( r.error )
+                            e.push($L(r.error));
+
+                        for(var k in r.field_errors )
+                            e.push($L(k + "-error: " + r.field_errors[k]));
+
+                        if( !e.length )
+                            e.push($L("Something went wrong with the task search ..."));
+
+                        if( f(e) )
+                            Mojo.Controller.errorDialog(e.join("... "));
+                    }
+
+                } else {
+                    Mojo.Log.info("TaskManager::searchTasks() sent [kinda bad]: r=%s", Object.toJSON(r));
+                    var e = ["Unknown error searching hiveminder tasks, huh"];
+
+                    if( f(e) )
+                        Mojo.Controller.errorDialog(e.join("... "));
+                }
+
+            } else {
+                Mojo.Log.info("TaskManager::searchTasks() sent [kinda bad]: transport=%s", Object.toJSON(transport));
+                var e = ["Unknown error searching hiveminder tasks -- host not found?"];
+
+                if( f(e) )
+                    Mojo.Controller.errorDialog(e.join("... "));
+            }
+
+        }.bind(this),
+
+        onFailure: function(transport) {
+            var t = new Template($L("Ajax Error: #{status}"));
+            var m = t.evaluate(transport);
+            var e = [m];
+
+            if( f(e) )
+                Mojo.Controller.errorDialog(e.join("... "));
+
+        }.bind(this)
+
+    });
+
+};
 
 Mojo.Log.info('loaded(TaskManager.js)');
