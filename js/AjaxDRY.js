5cb577fec097edfbf5c03c673e77658314150a60
Paul Miller <jettero@gmail.com>
2010-03-12 20:12:09 -0500

    DRY

---
+++ js/AjaxDRY.js
@@ -0,0 +1,51 @@
+
+function AjaxDRY(desc,url,method,params,success,failure) {
+
+    if( !success ) success = function() { return true; };
+    if( !failure ) failure = function() { return true; };
+
+    this.req = new Ajax.Request(url, {
+        method: method, parameters: params, evalJSON: true,
+
+        onSuccess: function(transport) {
+            if( transport.status >= 200 && transport.status < 300 ) {
+                Mojo.Log.info("%s ajax success transport=%s", desc, Object.toJSON(transport));
+
+                success(transport.responseJSON);
+
+            } else if( !transport.status ) {
+                Mojo.Log.info("%s ajax abort? transport=%s", desc, Object.toJSON(transport));
+
+                // this seems to be what happens on an abort
+
+            } else {
+                Mojo.Log.info("%s ajax mystery fail r=%s", desc, Object.toJSON(r));
+
+                if( failure() ) {
+                    var e = ["Unknown error issuing " + desc + " request"];
+
+                    Mojo.Controller.errorDialog(e.join("... "));
+                }
+            }
+
+        },
+
+        onFailure: function(transport) {
+            if( failure() ) {
+                var t = new Template("Ajax Error: #{status}");
+                var m = t.evaluate(transport);
+                var e = [m];
+
+                Mojo.Controller.errorDialog(e.join("... "));
+            }
+        }
+
+    });
+
+}
+
+AjaxDRY.prototype.abort = function() {
+
+    this.req.transport.abort();
+
+};
