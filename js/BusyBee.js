da507f14f10df8a26fc3157ab240340c65b9e6b4
Paul Miller <jettero@gmail.com>
2010-03-13 07:06:38 -0500

    say what is happening

--- js/BusyBee.js
+++ js/BusyBee.js
@@ -8,18 +8,24 @@ function BusyBee() {
 }
 
 BusyBee.prototype.busy = function(task) {
+    Mojo.Log.info("BusyBee::busy(%s)", task);
+
     this.tasks[task] = true;
 
     $("wait").setStyle({display: 'block'});
 };                                                                                                                             
 
 BusyBee.prototype.done = function(task) {
+    Mojo.Log.info("BusyBee::done(%s)", task);
 
     delete this.tasks[task];
 
-    for( var k in this.tasks )
+    for( var k in this.tasks ) {
+        Mojo.Log.info("BusyBee::done() [tasks remaining]");
         return;
+    }
 
+    Mojo.Log.info("BusyBee::done() [all done, hiding bee]");
     $("wait").setStyle({display: 'none'});
 };  
 
