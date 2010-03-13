a26903156f3717e0c956fe2f153d6e5454151c1c
Paul Miller <jettero@gmail.com>
2010-03-12 21:01:02 -0500

    I had to fix a couple things for jslint, taught jslint to not pester me about every fucking bitwise operator, and then adjusted this to my style a little

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -401,33 +401,33 @@ var cacheMaxAge = 4000;
 /*}}}*/
 
 /* {{{ */ TaskManager.prototype.fixutf8 = function (utftext) { // stolen from: http://www.webtoolkit.info/javascript-utf8.html
-    var string = "";
+    var str = "";
     var i = 0;
-    var c = c1 = c2 = 0;
+    var c,c1,c2,c3;
 
-    while ( i < utftext.length ) {
+    while( i<utftext.length ) {
 
         c = utftext.charCodeAt(i);
 
         if (c < 128) {
-            string += String.fromCharCode(c);
+            str += String.fromCharCode(c);
             i++;
-        }
-        else if((c > 191) && (c < 224)) {
+
+        } else if((c > 191) && (c < 224)) {
             c2 = utftext.charCodeAt(i+1);
-            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            str += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
             i += 2;
-        }
-        else {
+
+        } else {
             c2 = utftext.charCodeAt(i+1);
             c3 = utftext.charCodeAt(i+2);
-            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            str += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
             i += 3;
         }
 
     }
 
-    return string;
+    return str;
 };
 
 /*}}}*/
