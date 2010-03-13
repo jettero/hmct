7c2dbb64868fc68f598f51e619e43840fc77611d
Paul Miller <jettero@gmail.com>
2010-03-12 20:12:39 -0500

    it seems hiveminder is crushing the utf8 in the json

--- js/TaskManager.js
+++ js/TaskManager.js
@@ -153,7 +153,7 @@ var cacheMaxAge = 4000;
                     if( r.success ) {
                         Mojo.Log.info("TaskManager::searchTasks()::onSuccess() r.content.result=%s", r.content.result);
 
-                        me.setCache(search_key, (me.tasks = r.content.result.evalJSON()) );
+                        me.setCache(search_key, (me.tasks = me.fixutf8( r.content.result ).evalJSON()) );
                         me.processTasks();
 
                     } else {
@@ -433,6 +433,38 @@ var cacheMaxAge = 4000;
 
     for( var i=0; i<this.tasksChangeCallback.length; i++ )
         this.notifyTasksChangeStep(this.tasksChangeCallback[i]);
+};
+
+/*}}}*/
+
+/* {{{ */ TaskManager.prototype.fixutf8 = function (utftext) { // stolen from: http://www.webtoolkit.info/javascript-utf8.html
+    var string = "";
+    var i = 0;
+    var c = c1 = c2 = 0;
+
+    while ( i < utftext.length ) {
+
+        c = utftext.charCodeAt(i);
+
+        if (c < 128) {
+            string += String.fromCharCode(c);
+            i++;
+        }
+        else if((c > 191) && (c < 224)) {
+            c2 = utftext.charCodeAt(i+1);
+            string += String.fromCharCode(((c & 31) << 6) | (c2 & 63));
+            i += 2;
+        }
+        else {
+            c2 = utftext.charCodeAt(i+1);
+            c3 = utftext.charCodeAt(i+2);
+            string += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63));
+            i += 3;
+        }
+
+    }
+
+    return string;
 };
 
 /*}}}*/
