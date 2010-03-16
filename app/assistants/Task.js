85a5de0ed0ed975e242f05f531ff4d27ece81274
Paul Miller <jettero@gmail.com>
2010-03-15 20:44:10 -0400

    I think this is a start on how I'll layout the data...

--- app/assistants/Task.js
+++ app/assistants/Task.js
@@ -4,5 +4,21 @@ function TaskAssistant(_i) {
 }
 
 TaskAssistant.prototype.setup = function() {
+    Mojo.Log.info("Task()::setup()");
+
     $("id").update(this.item.record_locator);
+
+    this.tasksListAttrs = {
+        listTemplate:  'misc/naked-list-container',
+        emptyTemplate: 'misc/empty-list',
+        itemTemplate:  'misc/li-task-row',
+        swipeToDelete: false
+    };
+    this.tasksListModel = {listTitle: 'Hiveminder Tasks', items: [
+        {row_name: "test", row_data: "test test test test"},
+        {row_name: "test", row_data: "test test test test"},
+        {row_name: "test", row_data: "test test test test"},
+        {row_name: "test", row_data: "test test test test"}
+    ]};
+    this.controller.setupWidget('hm_task_list', this.tasksListAttrs, this.tasksListModel);
 };
