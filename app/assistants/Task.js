48c2dd05fd3f470bc1134863afcd9138efcf8e92
Paul Miller <jettero@gmail.com>
2010-03-16 20:56:28 -0400

    I may have stuffed the items into drawers... now how to get them to appear.  Honestly, this seems to be boring prototype.js .hide()/.show() ... I guess I expected the list widget to handle it for me

--- app/assistants/Task.js
+++ app/assistants/Task.js
@@ -19,17 +19,62 @@ TaskAssistant.prototype.setup = function() {
 
     this.tasksListModel = {listTitle: 'Hiveminder Tasks', items: [
 
-        {row_name: "test", category: "blarg", row_data: "test test test test"},
-        {row_name: "test", category: "blarg", row_data: "test test test test"},
-        {row_name: "test", category: "blarg", row_data: "test test test test"},
-        {row_name: "test", category: "blarg", row_data: "test test test test"},
-
-        {row_name: "test", category: "gralb", row_data: "test test test test"},
-        {row_name: "test", category: "gralb", row_data: "test test test test"},
-        {row_name: "test", category: "gralb", row_data: "test test test test"},
-        {row_name: "test", category: "gralb", row_data: "test test test test"}
+        {id: 1, row_name: "test", category: "blarg", row_data: "test test test test"},
+        {id: 2, row_name: "test", category: "blarg", row_data: "test test test test"},
+        {id: 3, row_name: "test", category: "blarg", row_data: "test test test test"},
+        {id: 4, row_name: "test", category: "blarg", row_data: "test test test test"},
+             
+        {id: 5, row_name: "test", category: "gralb", row_data: "test test test test"},
+        {id: 6, row_name: "test", category: "gralb", row_data: "test test test test"},
+        {id: 7, row_name: "test", category: "gralb", row_data: "test test test test"},
+        {id: 8, row_name: "test", category: "gralb", row_data: "test test test test"}
 
     ]};
 
     this.controller.setupWidget('hm_task_list', this.tasksListAttrs, this.tasksListModel);
+};
+
+TaskAssistant.prototype.activate = function() {
+    Mojo.Log.info("Task()::activate()");
+    var hrm = {blarg: true, gralb: true};
+    var me  = this;
+
+    for(var k in hrm) {
+        var compress     = this.controller.get('compress'     + k);
+        var compressable = this.controller.get('compressable' + k).hide();
+
+        compress.addClassName('compressor')
+        compress.compressorID = k;
+
+        this.controller.listen(compress, Mojo.Event.tap, this.clickCollapsibleList.bind(this, compressable, k));
+		this.tasksListModel.items.findAll(function(i){ return i.category === k }).each(function(i){
+
+            var compressable = me.controller.get('compressable' + i.category);
+                compressable.insert( me.controller.get('element' + i.id) );
+
+            me.controller.get('element' + i.id).show();
+
+        });
+    }
+};
+
+TaskAssistant.prototype.clickCollapsibleList = function(drawer, category, event) {
+    Mojo.Log.info("Task()::clickCollapsibleList()");
+
+	var targetRow = this.controller.get(event.target);
+    if (!targetRow.hasClassName("selection_target")) {
+        Mojo.Log.info("Task()::clickCollapsibleList() !selection_target" );
+        targetRow = targetRow.up('.selection_target');
+    }		
+
+    if (targetRow) {
+        var toggleButton = targetRow.down("div.arrow_button");
+
+        if (!toggleButton.hasClassName('palm-arrow-expanded') && !toggleButton.hasClassName('palm-arrow-closed'))
+            return;
+
+        var show = toggleButton.className;
+        Mojo.Log.info("Task()::clickCollapsibleList() open/close " + show );
+        this._toggleShowHideFolders(targetRow, this.controller.window.innerHeight, null, category);
+    }
 };
