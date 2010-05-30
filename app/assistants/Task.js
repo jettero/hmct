3d5e3b9fb9c2fc11dc0af50ca1596ee32dc0b033
Paul Miller <jettero@gmail.com>
2010-05-30 08:18:02 -0400

    there, add a click event to *this* since we don't get task ids in the and_then_html

--- app/assistants/Task.js
+++ app/assistants/Task.js
@@ -59,9 +59,9 @@ TaskAssistant.prototype.longTemplate  = new Mojo.View.Template(palmGetResource(M
         this.shortTemplate.evaluate(task) + this.longTemplate.evaluate(task)
     );
 
-    this.controller.get("task-snl").select(".and-then li").each(function(li){
-        li.setAttribute("x-mojo-tap-highlight", 'momentary');
-        li.addClassName("palm-row");
+    this.controller.get("task-snl").select(".and-then").each(function(x){
+        x.setAttribute("x-mojo-tap-highlight", 'momentary');
+        x.addClassName("palm-row");
         // li.observe('click', function(event){
         //     Event.element(event).
         // });
