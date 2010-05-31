d9cb635ffbd94cdbfb2d288936c747176a287aae
Paul Miller <jettero@gmail.com>
2010-05-31 08:44:29 -0400

    ok, this is the layout I'm going to use

--- app/assistants/Search.js
+++ app/assistants/Search.js
@@ -1,5 +1,24 @@
+/*jslint white: false, onevar: false, maxerr: 500000, regexp: false
+*/
+/*global Mojo $ OPT TMO
+*/
+
 function SearchAssistant() {
     Mojo.Log.info("Search()");
 }
+
+SearchAssistant.prototype.setup = function() {
+    Mojo.Log.info("Search::setup()");
+
+    var queryTextFieldAttributes = {
+        textCase:      Mojo.Widget.steModeLowerCase,
+        multiline:     false,
+        autoFocus:     false,
+        enterSubmits:  true
+    };
+
+    this.controller.setupWidget('query',     queryTextFieldAttributes, {});
+    this.controller.setupWidget('not-query', queryTextFieldAttributes, {});
+};
 
 Mojo.Log.info('loaded(Search.js)');
