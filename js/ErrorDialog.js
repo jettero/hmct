217ba259b385f1caace8f362f0e8ede5422336ae
Paul Miller <jettero@gmail.com>
2010-06-27 08:53:26 -0400

    hrm, it still wasn't quite right

--- js/ErrorDialog.js
+++ js/ErrorDialog.js
@@ -4,15 +4,10 @@
 */
 
 function ErrorDialog(launcher) {
-    this.t = new Template(launcher + " [#{desc}]: #{error}");
+    this.logTemplate = new Template(launcher + " ED:[#{desc}]: #{error}");
 }
 
 ErrorDialog.prototype.showError = function(desc,error) {
-    var errorText = this.t.evaluate({desc: desc, error: error});
-
-    Mojo.Log.error(errorText);
-
-    if( errorText.length > OPT.maxErrLen )
-         Mojo.Controller.errorDialog(errorText.substr(0, OPT.maxErrLen-4) + " ...");
-    else Mojo.Controller.errorDialog(errorText);
+    Mojo.Log.error( this.logTemplate.evaluate({desc: desc, error: error}) );
+     Mojo.Controller.errorDialog( error.length > OPT.maxErrLen ? error.substr(0, OPT.maxErrLen-4) + " ..." : error );
 };
