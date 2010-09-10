89509d0101afb23df2f92b276df45b97be1ed434
Paul Miller <jettero@gmail.com>
2010-09-10 18:13:21 -0400

    this is kinda neat, but I'm not happy with it at all — I have a better idea

--- app/assistants/NewTask.js
+++ app/assistants/NewTask.js
@@ -1,4 +1,42 @@
+/*jslint white: false, onevar: false, maxerr: 500000, regexp: false
+*/
+/*global Mojo
+*/
 
-function NewTaskAssistant() {
+function NewTaskAssistant(sceneAssistant) {
     Mojo.Log.info("NewTask()");
+
+    this.sceneAssistant = sceneAssistant;
+    this.controller     = sceneAssistant.controller;
+
 }
+
+NewTaskAssistant.prototype.setup = function(widget) {
+    Mojo.Log.info("NewTask::setup()");
+
+    this.widget = widget;
+
+    this.braindumpAttributes = {autoFocus: true};
+    this.appendAttributes    = {multiline: false, textCase: Mojo.Widget.steModeLowerCase, autoFocus: false};
+
+    this.braindumpModel = {};
+    this.appendModel    = {};
+
+    this.controller.setupWidget("braindump", this.braindumpAttributes, this.braindumpModel); 
+    this.controller.setupWidget("append",    this.appendAttributes,    this.appendModel); 
+
+    this.controller.setupWidget("go", {}, this.goModel = {buttonClass: 'affirmative', label: "Send"}); 
+    this.controller.setupWidget("no", {}, this.noModel = {buttonClass: 'negative',  label: "Cancel"}); 
+
+    Mojo.Event.listen($("go"), Mojo.Event.tap, this.go)
+    Mojo.Event.listen($("no"), Mojo.Event.tap, this.no)
+};
+
+
+NewTaskAssistant.prototype.go = function() {
+    Mojo.Log.info("NewTask::go()");
+};
+
+NewTaskAssistant.prototype.no = function() {
+    Mojo.Log.info("NewTask::no()");
+};
