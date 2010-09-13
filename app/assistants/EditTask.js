7624fbc1505d3bb4c1d01cff2df73053b39f13ee
Paul Miller <jettero@gmail.com>
2010-09-13 07:24:24 -0400

    a few more lines done

--- app/assistants/EditTask.js
+++ app/assistants/EditTask.js
@@ -17,22 +17,35 @@ function EditTaskAssistant(_i) {
 
 EditTaskAssistant.prototype.setup = function() {
     Mojo.Log.info("NewTask::setup()");
+    var t = this.task;
 
     this.menuSetup();
 
     this.s = new SuccessDialog("NewTask", this.controller);
     this.S = this.s.showSuccess;
 
-    this.controller.get("id").update(this.task.record_locator);
+    this.controller.get("id").update(t.record_locator);
 
     this.boringAttributes = {autoFocus: false, multiline: false, textCase: Mojo.Widget.steModeLowerCase};
-    this.controller.setupWidget("title", this.boringAttributes, this.titleModel = {value: this.task.summary});
+    this.controller.setupWidget("title", this.boringAttributes, this.titleModel = {value: t.summary});
 
     this.descriptionAttributes = {autoFocus: false, multiline: true, textCase: Mojo.Widget.steModeLowerCase};
-    this.controller.setupWidget("description", this.descriptionAttributes, this.descriptionModel = {value: this.task.description});
+    this.controller.setupWidget("description", this.descriptionAttributes, this.descriptionModel = {value: t.description});
 
     this.commentAttributes = {autoFocus: true, multiline: true, textCase: Mojo.Widget.steModeLowerCase};
     this.controller.setupWidget("comment", this.commentAttributes, this.commentModel = {});
+
+    this.controller.setupWidget("go", {}, this.goModel = {buttonClass: 'affirmative', label: "Update"});
+    this.controller.setupWidget("no", {}, this.noModel = {buttonClass: 'negative',  label: "Cancel"});
+
+    var checkBoxAttributes = { trueValue: '1', falseValue: '0' };
+    this.controller.setupWidget('stacks-up',      checkBoxAttributes, this.stacksUpModel      = {value: t.repeat_stacking});
+    this.controller.setupWidget('hidden-forever', checkBoxAttributes, this.hiddenForeverModel = {value: t.starts !== null ? "1":"0"});
+    this.controller.setupWidget('complete',       checkBoxAttributes, this.completeModel      = {value: t.complete});
+    this.controller.setupWidget('accept',         checkBoxAttributes, this.acceptModel        = {value: t.accepted});
+
+    if( this.task.accepted )
+        this.controller.get("accept-row").addClassName("generically-hidden");
 }
 
 Mojo.Log.info('loaded(EditTask.js)');
