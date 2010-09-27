e70dfea36603d5d330105812cb3270e2d9128ea2
Paul Miller <jettero@gmail.com>
2010-09-27 07:10:28 -0400

    filter outgoing traffic to cause network outages

---
+++ messup_inet.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+set -e
+
+echo 1. DROP hiveminder.org
+echo 2. REJECT hiveminder.org
+echo 3. clearup any trouble
+
+echo -n '[1-3] '
+read X
+
+old=$(sudo iptables --line -nL OUTPUT | grep hiveminder)
+
+if [ -n "$old" ]; then
+    sudo iptables -D OUTPUT $(echo $old | cut -d' ' -f1)
+fi
+
+if [ "$X" -eq 1 ]; then
+    sudo iptables -I OUTPUT -d hiveminder.org -j DROP -m comment --comment hiveminder
+fi
+
+if [ "$X" -eq 2 ]; then
+    sudo iptables -I OUTPUT -d hiveminder.org -j REJECT -m comment --comment hiveminder
+fi
+
+# if [ "$X" -eq 3 ]; then
+# fi
+
+set -x
+sudo iptables -nL | grep hiveminder
