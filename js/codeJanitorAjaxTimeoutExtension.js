81369087ae03e2192a59dbd5c174dcf3877769a4
Paul Miller <jettero@gmail.com>
2010-09-28 07:28:45 -0400

    lose the comments, fix whitespace, and REQ-ize this

--- js/codeJanitorAjaxTimeoutExtension.js
+++ js/codeJanitorAjaxTimeoutExtension.js
@@ -6,40 +6,39 @@
 
 function callInProgress (xmlhttp) {
     switch (xmlhttp.readyState) {
-        case 1: case 2: case 3:
-        return true;
-        break;
+        case 1:
+        case 2:
+        case 3:
+            return true;
+            break;
+
         // Case 4 and 0
         default:
-        return false;
-        break;
+            return false;
+            break;
     }
 }
 
-function showFailureMessage() {
-    alert('uh oh, it looks like the network is down. Try again shortly');
-}
-
-// Register global responders that will occur on all AJAX requests
 Ajax.Responders.register({
     onCreate: function(request) {
-        request['timeoutId'] = window.setTimeout(
-        function() {
-            // If we have hit the timeout and the AJAX request is active, abort it and let the user know
-            if (callInProgress(request.transport)) {
-                request.transport.abort();
-                showFailureMessage();
-                // Run the onFailure method if we set one up when creating the AJAX object
-                if (request.options['onFailure']) {
-                    request.options['onFailure'](request.transport, request.json);
+
+        request.timeoutId = window.setTimeout(
+
+            function() {
+                if (callInProgress(request.transport)) {
+
+                    request.transport.abort();
+
+                    if (request.options.onFailure)
+                        request.options.onFailure(request.transport, request.json);
                 }
-            }
-        },
-        5000 // Five seconds
+            },
+
+            OPT.ajaxTimeout
         );
     },
+
     onComplete: function(request) {
-        // Clear the timeout, the request completed ok
-        window.clearTimeout(request['timeoutId']);
+        window.clearTimeout(request.timeoutId);
     }
 });
