e69a33325c54d249ad9dc6a51f470c8f07f2f015
Paul Miller <jettero@gmail.com>
2010-10-13 06:59:55 -0400

    I think this should toggle the css correctly ... now, how to show what's keeping us busy?

--- js/BusyBee.js
+++ js/BusyBee.js
@@ -6,13 +6,20 @@
 function BusyBee() {
     this.tasks = {};
 
-    // Mojo.$("wait").observe('click', this.showDetail.bind(this));
+    Mojo.Event.listen($("wait"), Mojo.Event.tap, this.showDetail.bind(this));
 }
 
 BusyBee.prototype.showDetail = function() {
     Mojo.Log.info("BusyBee::showDetail()");
 
-    $("wait-detail").setStyle({display: 'block'});
+    if( this.showing ) {
+        $("wait-detail").setStyle({display: 'none'});
+        this.showing = false;
+
+    } else {
+        $("wait-detail").setStyle({display: 'block'});
+        this.showing = true;
+    }
 };
 
 BusyBee.prototype.busy = function(task) {
@@ -21,6 +28,8 @@ BusyBee.prototype.busy = function(task) {
     this.tasks[task] = true;
 
     $("wait").setStyle({display: 'block'});
+    if( this.showing )
+        $("wait-detail").setStyle({display: 'block'});
 };                                                                                                                             
 
 BusyBee.prototype.done = function(task) {
@@ -34,6 +43,7 @@ BusyBee.prototype.done = function(task) {
     }
 
     Mojo.Log.info("BusyBee::done() [all done, hiding bee]");
-    $("wait").setStyle({display: 'none'});
-};  
 
+    $("wait").setStyle({display: 'none'});
+    $("wait-detail").setStyle({display: 'none'});
+};  
