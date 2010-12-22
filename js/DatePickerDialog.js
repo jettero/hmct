4e10f9b7fc235e7341bda95decf818fb734bae9c
Paul Miller <jettero@gmail.com>
2010-12-21 22:08:47 -0500

    yeah, this rox utterly

--- js/DatePickerDialog.js
+++ js/DatePickerDialog.js
@@ -6,13 +6,23 @@ function DatePickerDialogAssistant(_sa,_mn) {
 
     this.scene = _sa;
     this.model = _sa[_mn];
+
+    this.pickedDate = this.pickedDate.bind(this);
 }
+
+DatePickerDialogAssistant.prototype.setup = function(_w) {
+    this.widget = _w;
+};
 
 DatePickerDialogAssistant.prototype.activate = function() {
     Mojo.Log.info("DatePicker::activate()");
 
-    this._datePicker = jQuery('#date-picker').datepicker({ inline: true });
+    this._datePicker = jQuery('#date-picker').datepicker({ inline: true, onSelect: this.pickedDate });
     this._datePicker.datepicker('setDate', this.model.value);
 };
 
-
+DatePickerDialogAssistant.prototype.pickedDate = function(dateText) {
+    setTimeout(function(){ this.widget.mojo.close(); }.bind(this), 200);
+    this.model.value = dateText;
+    this.scene.controller.modelChanged(this.model);
+};
