789510f8757da6c194fbbf216e3e45eeb9f42108
Paul Miller <jettero@gmail.com>
2010-12-21 21:57:29 -0500

    this is more like it

--- js/DatePickerDialog.js
+++ js/DatePickerDialog.js
@@ -1,17 +1,18 @@
 /*jslint white: false, onevar: false, maxerr: 500000, regexp: false
 */
 
-function DatePickerDialogAssistant(_sa,_cb) {
+function DatePickerDialogAssistant(_sa,_mn) {
     Mojo.Log.info("DatePicker()");
 
-    this.sceneAssistant = _sa;
-    this.pickedDate     = _cb || function() {};
+    this.scene = _sa;
+    this.model = _sa[_mn];
 }
 
 DatePickerDialogAssistant.prototype.activate = function() {
     Mojo.Log.info("DatePicker::activate()");
 
     this._datePicker = jQuery('#date-picker').datepicker({ inline: true });
+    this._datePicker.datepicker('setDate', this.model.value);
 };
 
 
