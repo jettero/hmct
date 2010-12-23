9aacd8aef2340040d97205371cf498ab6e0c834b
Paul Miller <jettero@gmail.com>
2010-12-22 19:32:59 -0500

    minor nits re: date pre-processing

--- js/DatePickerDialog.js
+++ js/DatePickerDialog.js
@@ -18,11 +18,21 @@ DatePickerDialogAssistant.prototype.activate = function() {
     Mojo.Log.info("DatePicker::activate()");
 
     this._datePicker = jQuery('#date-picker').datepicker({ inline: true, onSelect: this.pickedDate });
-    this._datePicker.datepicker('setDate', this.model.value);
+    if( this.model.value )
+         this._datePicker.datepicker('setDate', new Date(this.model.value));
+    else this._datePicker.datepicker('setDate', new Date());
 };
 
 DatePickerDialogAssistant.prototype.pickedDate = function(dateText) {
     setTimeout(function(){ this.widget.mojo.close(); }.bind(this), 200);
-    this.model.value = dateText;
+
+    var d = new Date(dateText);
+    var curr_date  = d.getDate();
+    var curr_month = d.getMonth();
+    var curr_year  = d.getFullYear();
+
+    curr_month ++; // month is 0-11 in this goofy object
+
+    this.model.value = [curr_year, curr_month, curr_date].join("-").replace(/-(\d)-/, "-0$1-").replace(/-(\d)$/, "-0$1");
     this.scene.controller.modelChanged(this.model);
 };
